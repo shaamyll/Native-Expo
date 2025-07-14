71e0723e006961dfe967edd2f4d26dba2dd9927d
shaamyll <you@example.com>
2025-07-14 19:04:02 +0530

    first commit

---
+++ Native expo/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}"
+  ],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors : {
+        primary: '#030014',
+      }
+    },
+  },
+  plugins: [],
+};
